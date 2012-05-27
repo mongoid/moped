3bd577743cb51412f22da67ab01190c349a49369
Durran Jordan <durran@gmail.com>
2012-05-27 23:37:45 +0200

    Flush out rdoc on database

--- lib/moped/database.rb
+++ lib/moped/database.rb
@@ -14,53 +14,85 @@ module Moped
   #   end
   class Database
 
-    # @return [Session] the database's session
-    attr_reader :session
-
-    # @return [String, Symbol] the database's name
-    attr_reader :name
-
-    # @param [Session] session the session
-    # @param [String, Symbol] name the database's name
-    def initialize(session, name)
-      @session = session
-      @name = name
-    end
+    # @attribute [r] name The name of the database.
+    # @attribute [r] session The session.
+    attr_reader :name, :session
 
     # Drop the database.
+    #
+    # @example Drop the database.
+    #   database.drop
+    #
+    # @return [ nil ] nil.
+    #
+    # @since 1.0.0
     def drop
       session.with(consistency: :strong) do |session|
-        session.context.command name, dropDatabase: 1
+        session.context.command(name, dropDatabase: 1)
       end
+    end
+
+    # Initialize the database.
+    #
+    # @example Initialize a database object.
+    #   Database.new(session, :artists)
+    #
+    # @param [ Session ] session The session.
+    # @param [ String, Symbol ] name The name of the database.
+    #
+    # @since 1.0.0
+    def initialize(session, name)
+      @session, @name = session, name
     end
 
     # Log in with +username+ and +password+ on the current database.
     #
-    # @param [String] username the username
-    # @param [String] password the password
+    # @example Authenticate against the database.
+    #   session.login("user", "pass")
+    #
+    # @param [ String ] username The username.
+    # @param [ String ] password The password.
+    #
+    # @since 1.0.0
     def login(username, password)
       session.context.login(name, username, password)
     end
 
     # Log out from the current database.
+    #
+    # @example Logout from the current database.
+    #   session.logout
+    #
+    # @since 1.0.0
     def logout
       session.context.logout(name)
     end
 
     # Run +command+ on the database.
     #
-    # @example
+    # @example Run a command.
     #   db.command(ismaster: 1)
     #   # => { "master" => true, hosts: [] }
     #
-    # @param [Hash] command the command to run
-    # @return [Hash] the result of the command
+    # @param [ Hash ] command The command to run.
+    #
+    # @return [ Hash ] the result of the command.
+    #
+    # @since 1.0.0
     def command(command)
       session.context.command name, command
     end
 
-    # @param [Symbol, String] collection the collection name
-    # @return [Moped::Collection] an instance of +collection+
+    # Get a collection by the provided name.
+    #
+    # @example Get a collection.
+    #   session[:users]
+    #
+    # @param [ Symbol, String ] collection The collection name.
+    #
+    # @return [ Collection ] An instance of the collection.
+    #
+    # @since 1.0.0
     def [](collection)
       Collection.new(self, collection)
     end
