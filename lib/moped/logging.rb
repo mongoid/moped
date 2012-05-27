957918e6a1f20298ebfd1cc32d69d1046703e6d9
Durran Jordan <durran@gmail.com>
2012-05-28 00:29:50 +0200

    Adding in rdoc on logger

--- lib/moped/logging.rb
+++ lib/moped/logging.rb
@@ -1,25 +1,58 @@
 module Moped
+
+  # Contains behaviour for logging.
   module Logging
+
+    # Get the logger.
+    #
+    # @example Get the logger.
+    #   Logging.logger
+    #
+    # @return [ Logger ] The logger.
+    #
+    # @since 1.0.0
     def logger
       return @logger if defined?(@logger)
-
       @logger = rails_logger || default_logger
     end
 
+    # Get the rails logger.
+    #
+    # @example Get the rails logger.
+    #   Logging.rails_logger
+    #
+    # @return [ Logger ] The Rails logger.
+    #
+    # @since 1.0.0
     def rails_logger
       defined?(Rails) && Rails.respond_to?(:logger) && Rails.logger
     end
 
+    # Get the default logger.
+    #
+    # @example Get the default logger.
+    #   Logging.default_logger
+    #
+    # @return [ Logger ] The default logger.
+    #
+    # @since 1.0.0
     def default_logger
       Logger.new(STDOUT).tap do |logger|
         logger.level = Logger::INFO
       end
     end
 
+    # Set the logger.
+    #
+    # @example Set the logger.
+    #   Logging.logger = logger
+    #
+    # @return [ Logger ] The logger.
+    #
+    # @since 1.0.0
     def logger=(logger)
       @logger = logger
     end
   end
-
   extend Logging
 end
