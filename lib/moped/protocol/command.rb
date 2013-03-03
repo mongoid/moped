b65c5f275d110d04e9fa093ef8ca09cabd98486d
Durran Jordan <durran@gmail.com>
2013-03-03 16:56:41 +0100

    Adding in Command#failure?

--- lib/moped/protocol/command.rb
+++ lib/moped/protocol/command.rb
@@ -7,15 +7,47 @@ module Moped
     # @example
     #   command = Moped::Protocol::Command.new :moped, ismaster: 1
     #   socket.write command.serialize
+    #
+    # @since 1.0.0
     class Command < Query
 
-      # @param [String, Symbol] database the database to run this command on
-      # @param [Hash] command the command to run
-      # @param [Hash] additional query options
+      # Determine if the provided reply message is a failure with respect to a
+      # command.
+      #
+      # @example Is the reply a command failure?
+      #   command.failure?(reply)
+      #
+      # @param [ Reply ] reply The reply to the command.
+      #
+      # @return [ true, false ] If the reply is a failure.
+      #
+      # @since 2.0.0
+      def failure?(reply)
+        reply.command_failure?
+      end
+
+      # Instantiate the new command.
+      #
+      # @example Instantiate the new command.
+      #   Moped::Protocol::Command.new(:moped_test, ismaster: 1)
+      #
+      # @param [ String, Symbol ] database The database to run the command on.
+      # @param [ Hash ] command The command to run.
+      # @param [ Hash ] options And additional query options.
+      #
+      # @since 1.0.0
       def initialize(database, command, options = {})
         super database, '$cmd', command, options.merge(limit: -1)
       end
 
+      # Provide the value that will be logged when the command runs.
+      #
+      # @example Provide the log inspection.
+      #   command.log_inspect
+      #
+      # @return [ String ] The string value for logging.
+      #
+      # @since 1.0.0
       def log_inspect
         type = "COMMAND"
         "%-12s database=%s command=%s" % [type, database, selector.inspect]
--- spec/moped/protocol/command_spec.rb
+++ spec/moped/protocol/command_spec.rb
@@ -2,22 +2,55 @@ require "spec_helper"
 
 describe Moped::Protocol::Command do
 
+  describe "#failure?" do
+
+    let(:command) do
+      described_class.new(:moped, ismaster: 1)
+    end
+
+    let(:reply) do
+      Moped::Protocol::Reply.new
+    end
+
+    context "when the reply is a command failure" do
+
+      before do
+        reply.documents = [{ "ok" => 0.0 }]
+      end
+
+      it "returns true" do
+        expect(command).to be_failure(reply)
+      end
+    end
+
+    context "when the reply is not a command failure" do
+
+      before do
+        reply.documents = [{ "ok" => 1.0 }]
+      end
+
+      it "returns true" do
+        expect(command).to_not be_failure(reply)
+      end
+    end
+  end
+
   describe "#initialize" do
+
     let(:command) do
       described_class.new(:moped, ismaster: 1)
     end
 
     it "sets the query's full collection name" do
-      command.full_collection_name.should eq "moped.$cmd"
+      expect(command.full_collection_name).to eq("moped.$cmd")
     end
 
     it "sets the query's selector to the command provided" do
-      command.selector.should eq(ismaster: 1)
+      expect(command.selector).to eq(ismaster: 1)
     end
 
     it "sets the query's limit to -1" do
-      command.limit.should eq(-1)
+      expect(command.limit).to eq(-1)
     end
   end
-
 end
