4729c428f663bfbbbcefbff8c690da35ba2929fa
Durran Jordan <durran@gmail.com>
2013-03-11 13:17:07 +0100

    Start flushing out the write abstraction

--- lib/moped/operation/write.rb
+++ lib/moped/operation/write.rb
@@ -2,18 +2,47 @@
 module Moped
   module Operation
 
+    # Encapsulates behaviour for write operations.
+    #
+    # @since 2.0.0
     class Write
 
-      # The operation can be a Protocol::Insert, Protocol::Update,
-      # Protocol::Delete
-      def initialize(operation, write_concern)
+      # @!attribute concern
+      #   @return [ Object ] The configured write concern.
+      # @!attribute database
+      #   @return [ String ] The database the read is from.
+      # @!attribute operation
+      #   @return [ Protocol::Insert, Protocol::Update, Protocol::Delete ]
+      #     The write operation.
+      attr_reader :concern, :database, :operation
+
+      # Instantiate the write operation.
+      #
+      # @example Instantiate the write.
+      #   Write.new(insert)
+      #
+      # @param [ Protocol::Insert, Protocol::Update, Protocol::Delete ] operation
+      #   The write operation.
+      #
+      # @since 2.0.0
+      def initialize(operation, concern)
         @operation = operation
         @database = operation.database
-        @write_concern = write_concern
+        @concern = concern
       end
 
+      # Execute the write operation on the provided node. If the write concern
+      # is propagating, then the gle command will be piggybacked onto the
+      # initial write operation.
+      #
+      # @example Execute the operation.
+      #   write.execute(node)
+      #
+      # @param [ Node ] node The node to execute the write on.
+      #
+      # @since 2.0.0
       def execute(node)
-        message = operation.piggyback(write_concern.command(database))
+        message = operation.piggyback(concern.command(database))
         node.process(message)
       end
     end
