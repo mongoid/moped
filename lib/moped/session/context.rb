f67f42753259c582d55845ec835452686d2a6510
Durran Jordan <durran@gmail.com>
2013-03-14 11:20:41 +0100

    Remove the dead code in context

--- lib/moped/session/context.rb
+++ lib/moped/session/context.rb
@@ -31,28 +31,34 @@ module Moped
       end
 
       def logout(database)
-        cluster.auth.delete database.to_s
+        cluster.auth.delete(database.to_s)
       end
 
       def query(database, collection, selector, options = {})
+        # @todo: This goes away.
         if consistency == :eventual
           options[:flags] ||= []
           options[:flags] |= [:slave_ok]
         end
-
         with_node do |node|
           node.query(database, collection, selector, options)
         end
+        # query = Protocol::Query.new(database, collection, selector, options)
+        # Operation::Read.new(query).execute(read_preference.select(ring))
       end
 
       def command(database, command)
+        # @todo: This goes away.
         options = consistency == :eventual ? { :flags => [:slave_ok] } : {}
         with_node do |node|
           node.command(database, command, options)
         end
+        # command = Protocol::Command.new(database, cmd, options)
+        # Operation::Read.new(command).execute(read_preference.select(ring))
       end
 
       def insert(database, collection, documents, options = {})
+        # @todo: This goes away.
         with_node do |node|
           if safe?
             node.pipeline do
@@ -63,9 +69,12 @@ module Moped
             node.insert(database, collection, documents, options)
           end
         end
+        # insert = Protocol::Insert.new(database.name, name, documents, options)
+        # Operation::Write.new(insert, concern).execute(ring.next_primary)
       end
 
       def update(database, collection, selector, change, options = {})
+        # @todo: This goes away.
         with_node do |node|
           if safe?
             node.pipeline do
@@ -76,9 +85,12 @@ module Moped
             node.update(database, collection, selector, change, options)
           end
         end
+        # update = Protocol::Update.new(database, collection, selector, change, options))
+        # Operation::Write.new(update, concern).execute(ring.next_primary)
       end
 
       def remove(database, collection, selector, options = {})
+        # @todo: This goes away.
         with_node do |node|
           if safe?
             node.pipeline do
@@ -89,16 +101,11 @@ module Moped
             node.remove(database, collection, selector, options)
           end
         end
+        # delete = Protocol::Delete.new(database, collection, selector, options)
+        # Operation::Write.new(delete, concern).execute(ring.next_primary)
       end
 
-      def get_more(*args)
-        raise NotImplementedError, "#get_more cannot be called on Context; it must be called directly on a node"
-      end
-
-      def kill_cursors(*args)
-        raise NotImplementedError, "#kill_cursors cannot be called on Context; it must be called directly on a node"
-      end
-
+      # @todo: This goes away.
       def with_node
         if consistency == :eventual
           cluster.with_secondary do |node|
