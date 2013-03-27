1de6d2ac18b3a4f206aaf4f03e96bb2f5b5cd391
Jonathan Hyman <jonathan.hyman@post.harvard.edu>
2013-03-27 10:38:42 -0400

    Adds checking for the word 'unauthorized' to the unauthorized check. #171

--- spec/moped/protocol/reply_spec.rb
+++ spec/moped/protocol/reply_spec.rb
@@ -316,6 +316,24 @@ describe Moped::Protocol::Reply do
       end
     end
 
+    context "when the error message says unauthorized" do
+      let(:error) do
+        { "ok" => 0, "err" => "unauthorized", "assertionCode" => 2004 }
+      end
+
+      let(:reply) do
+        described_class.new
+      end
+
+      before do
+        reply.documents = [ error ]
+      end
+
+      it "returns true" do
+        reply.should be_unauthorized
+      end
+    end
+
     context "when no auth errors exist" do
 
       let(:error) do
