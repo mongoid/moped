ac0c5dd18a0e9738ff3440cb3a88c39bdd2cc0b5
Jonathan Hyman <jonathan.hyman@post.harvard.edu>
2014-07-29 10:59:31 -0400

    Adds a retry for 15986 error code due to replica set reconfiguring.

--- lib/moped/errors.rb
+++ lib/moped/errors.rb
@@ -100,7 +100,7 @@ module Moped
     class PotentialReconfiguration < MongoError
 
       # Not master error codes.
-      NOT_MASTER = [ 13435, 13436, 10009]
+      NOT_MASTER = [ 13435, 13436, 10009, 15986]
 
       # Error codes received around reconfiguration
       CONNECTION_ERRORS_RECONFIGURATION = [ 15988, 10276, 11600, 9001, 13639, 10009 ]
