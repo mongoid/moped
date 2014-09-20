5c2a17907a5aa88173a54ed96c10a13cc0ebfa5d
y8 <y8@ya.ru>
2014-09-21 00:45:18 +0200

    If there no ActiveSupport just redefine :to_json

--- lib/moped/log_format/shell_format/core_ext/bson/object_id.rb
+++ lib/moped/log_format/shell_format/core_ext/bson/object_id.rb
@@ -1,7 +1,7 @@
 module Moped
   module ShellFormatPatch
     module ObjectId
-      def to_mongo_shell
+      def to_mongo_shell(*args)
         UnsafeJSONString.new(%Q{ObjectId("#{to_s}")})
       end
 
@@ -13,10 +13,23 @@ module Moped
         end
       end
 
+      def to_json_with_mongo_shell(*args)
+        if args.first && args.first[:mongo_shell_format]
+          to_mongo_shell
+        else
+          to_json_without_mongo_shell(*args)
+        end
+      end
+
       def self.included(base)
         base.class_eval do
-          alias_method :as_json_without_mongo_shell, :as_json
-          alias_method :as_json, :as_json_with_mongo_shell
+          if defined? ActiveSupport
+            alias_method :as_json_without_mongo_shell, :as_json
+            alias_method :as_json, :as_json_with_mongo_shell
+          else
+            alias_method :to_json_without_mongo_shell, :to_json
+            alias_method :to_json, :to_json_with_mongo_shell
+          end
         end
       end
     end
